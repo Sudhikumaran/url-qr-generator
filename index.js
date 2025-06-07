5b2f2e79dde0195c1cb9b3f593d8d52b3684a85a
Sudhikumaran <147371824+Sudhikumaran@users.noreply.github.com>
2025-06-07 20:47:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    const qr_svg = qr.image(url, { type: "png" });
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  });
